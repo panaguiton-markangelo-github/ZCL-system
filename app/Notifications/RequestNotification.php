9d829efcad03ab225ec79139d854e39e17c1f762
MARK ANGELO SANTOS PANAGUITON <61976120+kookie1721@users.noreply.github.com>
2023-01-30 01:12:29 +0800

    updates

--- app/Notifications/RequestNotification.php
+++ app/Notifications/RequestNotification.php
@@ -31,7 +31,7 @@ class RequestNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail', 'database'];
+        return ['database'];
     }
 
     /**
